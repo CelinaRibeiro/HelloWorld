e4d7e9e71f148f9d9a1b59a81eaa74649b107bab
Celina Ferreira Ribeiro <ribeiro.f.celina@gmail.com>
2018-05-28 11:46:01 -0300

    Primeiro commit github.

---
+++ src/br/com/classe/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package br.com.classe;
+
+/**
+ * @author Celina
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World.");
+
+	}
+
+}
